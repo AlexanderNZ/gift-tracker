db3e9f006807323f4df0f84c9673bd78acf71ebc
Alex Corkin <alexcorkin@gmail.com>
2017-01-18 09:56:09 +1300

    add encoding for ugly list

--- src/app/export-file-generator/export-file-generator.component.ts
+++ src/app/export-file-generator/export-file-generator.component.ts
@@ -28,8 +28,15 @@ export class ExportFileGeneratorComponent implements OnInit {
   }
 
   saveUgly(){
-    let blob = new Blob(["Hello, ugly!"], {type: "text/plain;charset=utf-8"});
-    fileSaver.saveAs(blob, "encoded-christmas-list.txt");
+
+    let fileContent = "My Christmas List \n\n";
+    for (let i = 0; i < this.trackedPeopleService.person.length; i++){
+      fileContent += "" + this.trackedPeopleService.person[i].toString() + "\n";
+    }
+
+    let encodedString = btoa(fileContent);
+    let blob = new Blob([encodedString], {type: "text/plain;charset=utf-8"});
+    fileSaver.saveAs(blob, "christmas-list.txt");
   }
 
 }
